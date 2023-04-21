3626aa3a1f905a06a2ba0808cfd1808578037751
Do Duc Manh <89192065+ongimkshp@users.noreply.github.com>
2023-04-21 13:27:37 +0700

    Fix deploy http

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>{{config('l5-swagger.documentations.'.$documentation.'.api.title')}}</title>
-    <link rel="stylesheet" type="text/css" href="{{ l5_swagger_asset($documentation, 'swagger-ui.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_url(l5_swagger_asset($documentation, 'swagger-ui.css')) }}">
     <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-32x32.png') }}" sizes="32x32"/>
     <link rel="icon" type="image/png" href="{{ l5_swagger_asset($documentation, 'favicon-16x16.png') }}" sizes="16x16"/>
     <style>
@@ -30,8 +30,8 @@
 <body>
 <div id="swagger-ui"></div>
 
-<script src="{{ l5_swagger_asset($documentation, 'swagger-ui-bundle.js') }}"></script>
-<script src="{{ l5_swagger_asset($documentation, 'swagger-ui-standalone-preset.js') }}"></script>
+<script src="{{ secure_url(l5_swagger_asset($documentation, 'swagger-ui-bundle.js')) }}"></script>
+<script src="{{ secure_url(l5_swagger_asset($documentation, 'swagger-ui-standalone-preset.js')) }}"></script>
 <script>
     window.onload = function() {
         // Build a system
